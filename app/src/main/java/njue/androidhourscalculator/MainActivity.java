cb7620823c21a41fbc238f171e246e8c782af44e
Anthony <anthonyschultz1997@gmail.com>
2018-05-23 13:31:50 +1000

    Uploading android project

--- app/src/main/java/njue/androidhourscalculator/MainActivity.java
+++ app/src/main/java/njue/androidhourscalculator/MainActivity.java
@@ -7,7 +7,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    private int state = 0; //0 is nothing entered, 1 is 1 number, 2 is 2 numbers entered.
+    private int state; //0 is nothing entered, 1 is 1 number, 2 is 2 numbers entered. 3 is 2 numbers plus the unit of time. 4 is 2 numbers plus the unit of time and plus/minus
+    private int totalSeconds;
+    private int currentSeconds;
     private TextView current;
     private TextView total;
 
@@ -16,74 +18,164 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        state = 0;
+        current = findViewById(R.id.textViewCurrent);
+        total = findViewById(R.id.textViewTotal);
     }
 
     private void onClick(View view)
     {
         if (view.getId() == R.id.button0)
         {
-
-            state++;
+            if (state == 0)
+            {
+                addFirstNumber(0);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(0);
+            }
         }
         if (view.getId() == R.id.button1)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(1);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(1);
+            }
         }
         if (view.getId() == R.id.button2)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(2);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(2);
+            }
         }
         if (view.getId() == R.id.button3)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(3);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(3);
+            }
         }
         if (view.getId() == R.id.button4)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(4);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(4);
+            }
         }
         if (view.getId() == R.id.button5)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(5);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(5);
+            }
         }
         if (view.getId() == R.id.button6)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(6);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(6);
+            }
         }
         if (view.getId() == R.id.button7)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(7);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(7);
+            }
         }
         if (view.getId() == R.id.button8)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(8);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(8);
+            }
         }
         if (view.getId() == R.id.button9)
         {
-
+            if (state == 0)
+            {
+                addFirstNumber(9);
+            }
+            if (state == 1)
+            {
+                addSecondNumber(9);
+            }
         }
         if (view.getId() == R.id.buttonMinus)
         {
-
+            if (state == 3)
+            {
+                plusMinus(1);
+            }
         }
         if (view.getId() == R.id.buttonPlus)
         {
-
+            if (state == 3)
+            {
+                plusMinus(2);
+            }
         }
         if (view.getId() == R.id.buttonEquals)
         {
-
+            if (state == 4)
+            {
+                equalSign();
+            }
         }
         if (view.getId() == R.id.buttonHours)
         {
-
+            if (state == 2)
+            {
+                addTimeUnit(1);
+            }
         }
         if (view.getId() == R.id.buttonMinutes)
         {
-
+            if (state == 2)
+            {
+                addTimeUnit(2);
+            }
         }
         if (view.getId() == R.id.buttonSeconds)
         {
-
+            if (state == 2)
+            {
+                addTimeUnit(3);
+            }
         }
         if (view.getId() == R.id.buttonClear)
         {
@@ -91,5 +183,68 @@ public class MainActivity extends AppCompatActivity
         }
 
     }
+
+    private void addFirstNumber(int buttonPressed)
+    {
+        current.setText(buttonPressed);
+        state = 1;
+    }
+
+    private void addSecondNumber(int buttonPressed)
+    {
+        String previousValue = current.getText().toString();
+        current.setText(buttonPressed + previousValue);
+        state = 2;
+    }
+
+    //1 = Hours, 2 = Minutes, 3 = Seconds
+    private void addTimeUnit(int timeUnit)
+    {
+        String previousValue = current.getText().toString();
+        int previousValueInt = Integer.getInteger(previousValue);
+        if (timeUnit == 1)
+        {
+            current.setText(previousValue + "H");
+            currentSeconds = previousValueInt;
+        }
+        if (timeUnit == 2)
+        {
+            current.setText(previousValue + "M");
+            currentSeconds = previousValueInt * 60;
+        }
+        if (timeUnit == 3)
+        {
+            current.setText(previousValue + "S");
+            currentSeconds = previousValueInt * 60 * 60;
+        }
+        state = 3;
+    }
+
+    //1 = +, 2 = -
+    private void plusMinus(int sign)
+    {
+        String previousValue = current.getText().toString();
+        if (sign == 1)
+        {
+            totalSeconds = totalSeconds + currentSeconds;
+            current.setText("+" + previousValue);
+        }
+        if (sign == 2)
+        {
+            totalSeconds = totalSeconds - currentSeconds;
+            current.setText("-" + previousValue);
+        }
+        currentSeconds = 0;
+
+        state = 4;
+    }
+
+    private void equalsSign()
+    {
+        //calculateTime();
+    }
+
+
+
 
 }
